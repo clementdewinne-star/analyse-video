29985dc1e086c673b582079975f949b0ea4c0739
Clement <ton.email@exemple.com>
2025-11-21 20:07:13 -0500

    Ajout Collectstatic

--- build.sh
+++ build.sh
@@ -5,11 +5,11 @@ set -o errexit
 # 1. Installer les ingrédients
 pip install -r requirements.txt
 
-# 2. RASSEMBLER LE DESIGN (C'est la ligne qui manquait !)
+# 2. RASSEMBLER LE DESIGN (La ligne magique ✨)
 python manage.py collectstatic --no-input
 
 # 3. Construire la base de données
 python manage.py migrate
 
-# 4. Créer le Superuser automatiquement (si besoin)
+# 4. Créer le Superuser (Admin)
 python manage.py shell -c "from django.contrib.auth import get_user_model; User = get_user_model(); User.objects.filter(username='admin').exists() or User.objects.create_superuser('admin', 'admin@example.com', 'admin123')"
