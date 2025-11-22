41a58c4731d14546f8ffd870e668d6872274ae95
Clement <ton.email@exemple.com>
2025-11-21 19:09:25 -0500

    Correction Mac

--- backend/settings.py
+++ backend/settings.py
@@ -26,6 +26,8 @@ SECRET_KEY = 'django-insecure-#b(tqc%v+k)528gfj#7sb_nx4*7eh-gvpzft7!!l+mpns275el
 DEBUG = True
 
 ALLOWED_HOSTS = []
+# Correction pour Render
+CSRF_TRUSTED_ORIGINS = ['https://*.onrender.com']
 
 
 # Application definition
